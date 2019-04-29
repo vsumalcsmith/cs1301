88b6454bbaf762e25d615c47453046ce1c6c7991
vsumalcsmith <44275927+vsumalcsmith@users.noreply.github.com>
2019-04-29 08:10:49 -0400

    Create TestVoid.java

---
+++ TestVoid.java
@@ -0,0 +1,23 @@
+public class TestVoid{
+   public static void main(String[] args){
+   System.out.print("The grade is ");
+   PrintGrade(78);
+   System.out.print("The grade is ");
+   PrintGrade(56);
+   }
+   
+   
+public static void PrintGrade(double score)
+{
+ if(score >= 90)
+    System.out.println('A');
+ else if(score >= 80)
+    System.out.println('B');
+ else if(score >= 70)
+    System.out.println('C');
+ else if(score >= 60)
+    System.out.println('D');
+ else
+    System.out.println('F');
+}
+}
